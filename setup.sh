2613010a08b98fae2e9df0622c205d4d3f26d722
Yoaquim Cintrón <yoaquim@outlook.com>
2015-10-09 03:23:30 -0400

    Add setup script to create all symlinks

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ln -s "~/.dotfiles/.bash_profile ~/.bash_profile"
+
+ln -s "~/.dotfiles/.bash_profile ~/.bashrc"
+
+ln -s "~/.dotfiles/.gitconfig ~/.gitconfig"
+
+ln -s "~/.dotfiles/.tmux.conf ~/.tmux.conf"
+
+ln -s "~/.dotfiles/.config ~/.config"
+
+ln -s "~/.dotfiles/.slate.js ~/.slate.js"
