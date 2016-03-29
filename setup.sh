21b0d77da7ff4ee4b769964d443e2286dfe6436e
Yoaquim Cintrón <yoaquim@outlook.com>
2016-03-29 12:13:16 -0400

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-ln -s "~/.dotfiles/.bash_profile ~/.bash_profile"
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 
-ln -s "~/.dotfiles/.bash_profile ~/.bashrc"
+ln -s ~/.dotfiles/.bash_profile ~/.bashrc
 
-ln -s "~/.dotfiles/.gitconfig ~/.gitconfig"
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
-ln -s "~/.dotfiles/.tmux.conf ~/.tmux.conf"
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
-ln -s "~/.dotfiles/.config ~/.config"
+ln -s ~/.dotfiles/.config ~/.config
 
-ln -s "~/.dotfiles/.slate.js ~/.slate.js"
+ln -s ~/.dotfiles/.slate.js ~/.slate.js
 
-ln -s "~/.dotfiles/.ideavimrc ~/.ideavimrc"
+ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
