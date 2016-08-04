006fadfc3aaf7b0140a9dcbabd294033155b8a18
Yoaquim Cintrón <yoaquim@outlook.com>
2016-08-03 23:15:27 -0400

    Fixing config

--- setup.sh
+++ setup.sh
@@ -1,20 +1,54 @@
 #!/bin/sh
-ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 
-ln -s ~/.dotfiles/bash_profile ~/.bashrc
+function remove_symlinks {
+    rm ~/.bash_profile
 
-ln -s ~/.dotfiles/bash_git ~/.bash_git
+    rm ~/.bashrc
 
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+    rm ~/.bash_git
 
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+    rm ~/.gitconfig
 
-ln -s ~/.dotfiles/config ~/.config
+    rm ~/.tmux.conf
 
-ln -s ~/.dotfiles/cripts ~/.scripts
+    rm -rf ~/.config
 
-ln -s ~/.dotfiles/slate.js ~/.slate.js
+    rm -rf ~/.scripts
 
-ln -s ~/.dotfiles/ideavimrc ~/.ideavimrc
+    rm ~/.slate.js
 
-ln -s ~/.dotfiles/spacemacs ~/.spacemacs
+    rm ~/.ideavimrc
+
+    rm ~/.spacemacs
+}
+
+function link_symlinks {
+    ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+
+    ln -s ~/.dotfiles/bash_profile ~/.bashrc
+
+    ln -s ~/.dotfiles/bash_git ~/.bash_git
+
+    ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+
+    ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+
+    ln -s ~/.dotfiles/config ~/.config
+
+    ln -s ~/.dotfiles/cripts ~/.scripts
+
+    ln -s ~/.dotfiles/slate.js ~/.slate.js
+
+    ln -s ~/.dotfiles/ideavimrc ~/.ideavimrc
+
+    ln -s ~/.dotfiles/spacemacs ~/.spacemacs
+}
+
+if [ -z  "$1" ]; then
+    link_symlinks
+else
+    if [ $1="f" ]; then
+        remove_symlinks
+        link_symlinks
+    fi
+fi
