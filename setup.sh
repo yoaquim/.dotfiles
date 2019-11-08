2d055391ac24bf492137f107e09fd67db37c2b10
Yoaquim Cintrón <yoaquim@outlook.com>
2019-11-08 16:09:38 -0400

    Updarte

--- setup.sh
+++ setup.sh
@@ -1,54 +1,36 @@
 #!/bin/sh
 
-function remove_symlinks {
+remove_symlinks() {
     rm ~/.bash_profile
-
     rm ~/.bashrc
-
     rm ~/.bash_git
-
     rm ~/.gitconfig
-
     rm ~/.tmux.conf
-
     rm -rf ~/.config
-
-    rm -rf ~/.scripts
-
     rm ~/.slate.js
-
     rm ~/.ideavimrc
-
     rm ~/.spacemacs
+    rm ~/.yabairc
 }
 
-function link_symlinks {
+link_symlinks() {
     ln -s ~/.dotfiles/bash_profile ~/.bash_profile
-
     ln -s ~/.dotfiles/bash_profile ~/.bashrc
-
     ln -s ~/.dotfiles/bash_git ~/.bash_git
-
     ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-
     ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-
     ln -s ~/.dotfiles/config ~/.config
-
-    ln -s ~/.dotfiles/scripts ~/.scripts
-
     ln -s ~/.dotfiles/slate.js ~/.slate.js
-
     ln -s ~/.dotfiles/ideavimrc ~/.ideavimrc
-
     ln -s ~/.dotfiles/spacemacs ~/.spacemacs
+    ln -s ~/.dotfiles/yabairc ~/.yabairc
 }
 
 if [ -z  "$1" ]; then
     link_symlinks
+elif [ "${1}" = "f" ]; then
+    remove_symlinks
+    link_symlinks
 else
-    if [ $1="f" ]; then
-        remove_symlinks
-        link_symlinks
-    fi
+    echo -e "\n\tOnly accepted flag if 'f'\n"
 fi
