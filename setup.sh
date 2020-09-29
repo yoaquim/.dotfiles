5f909947674682eb4d4b3cbe812d32a721a22021
Yoaquim Cintrón <yoaquim@outlook.com>
2020-09-29 13:14:09 -0400

    Update setup script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ remove_symlinks() {
     rm ~/.bash_profile
     rm ~/.bashrc
     rm ~/.bash_git
+    rm ~/.bash_aliases
     rm ~/.gitconfig
     rm ~/.tmux.conf
     rm -rf ~/.config
@@ -14,8 +15,10 @@ remove_symlinks() {
     rm ~/.skhdrc
 }
 
-link_symlinks() {
-    ln -s ~/.dotfiles/bash_git ~/.bash_git
+link_mac_symlinks() {
+    ln -s ~/.dotfiles/bashrc_mac ~/.bash_profile
+    ln -s ~/.dotfiles/bashrc_mac ~/.bashrc
+    ln -s ~/.dotfiles/bash_git_mac ~/.bash_git
     ln -s ~/.dotfiles/bash_aliases ~/.bash_aliases
     ln -s ~/.dotfiles/gitconfig ~/.gitconfig
     ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
@@ -27,11 +30,35 @@ link_symlinks() {
     ln -s ~/.dotfiles/yabairc ~/.skhdrc
 }
 
-if [ -z  "$1" ]; then
-    link_symlinks
-elif [ "${1}" = "f" ]; then
+link_linux_symlinks() {
+    ln -s ~/.dotfiles/bashrc_linux ~/.bash_profile
+    ln -s ~/.dotfiles/bashrc_linux ~/.bashrc
+    ln -s ~/.dotfiles/bash_git_linux ~/.bash_git
+    ln -s ~/.dotfiles/bash_aliases ~/.bash_aliases
+    ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+    ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+    ln -s ~/.dotfiles/config ~/.config
+    ln -s ~/.dotfiles/slate.js ~/.slate.js
+    ln -s ~/.dotfiles/ideavimrc ~/.ideavimrc
+    ln -s ~/.dotfiles/spacemacs ~/.spacemacs
+    ln -s ~/.dotfiles/yabairc ~/.yabairc
+    ln -s ~/.dotfiles/yabairc ~/.skhdrc
+}
+
+if [ "${2}" = "-f" ]; then
     remove_symlinks
-    link_symlinks
-else
-    echo -e "\n\tOnly accepted flag if 'f'\n"
+elif [ ! -z "${2}" ] && [ "${2}" != "-f" ]; then
+    echo "\n\tOnly accepted flag for second param is 'f'\n"
+    exit 1
 fi
+
+if [ -z "${1}" ]; then
+    echo "\n\tNeed to specify which OS, '-m' for Mac, '-l' for Linux\n"
+elif [ "${1}" = "-m" ]; then
+    link_mac_symlinks
+elif [ "${1}" = "-l" ]; then
+    link_linux_symlinks
+else
+    echo "\n\tValid flags for first param are 'l' or 'm'\n"
+fi
+
