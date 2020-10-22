74f045dfcd5ea522c480233d62bf2c5559f721ab
Yoaquim Cintrón <yoaquim@outlook.com>
2020-10-22 02:44:45 -0400

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,52 +1,31 @@
 #!/bin/sh
 
-remove_symlinks() {
-    pushd ~
-    rm ~/.bash_profile
-    rm ~/.bashrc
-    rm ~/.bash_git
-    rm ~/.bash_aliases
-    rm ~/.gitconfig
-    rm ~/.tmux.conf
-    rm ~/.slate.js
-    rm ~/.ideavimrc
-    rm ~/.spacemacs
-    rm ~/.yabairc
-    popd
-}
-
 link_mac_symlinks() {
-    ln -s ~/.dotfiles/bashrc/bashrc_mac ~/.bash_profile
-    ln -s ~/.dotfiles/bashrc/bashrc_mac ~/.bashrc
-    ln -s ~/.dotfiles/bashrc/bash_git_mac ~/.bash_git
-    ln -s ~/.dotfiles/bashrc/bash_aliases ~/.bash_aliases
-    ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-    ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-    ln -s ~/.dotfiles/slate.js ~/.slate.js
-    ln -s ~/.dotfiles/ideavimrc ~/.ideavimrc
-    ln -s ~/.dotfiles/spacemacs ~/.spacemacs
-    ln -s ~/.dotfiles/yabairc ~/.yabairc
+    ln -sf ~/.dotfiles/bashrc/bashrc_mac ~/.bash_profile
+    ln -sf ~/.dotfiles/bashrc/bashrc_mac ~/.bashrc
+    ln -sf ~/.dotfiles/bashrc/bash_git_mac ~/.bash_git
+    ln -sf ~/.dotfiles/bashrc/bash_aliases ~/.bash_aliases
+    ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+    ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+    ln -sf ~/.dotfiles/slate.js ~/.slate.js
+    ln -sf ~/.dotfiles/ideavimrc ~/.ideavimrc
+    ln -sf ~/.dotfiles/spacemacs ~/.spacemacs
+    ln -sf ~/.dotfiles/yabairc ~/.yabairc
 }
 
 link_linux_symlinks() {
-    ln -s ~/.dotfiles/bash/bashrc_linux ~/.bash_profile
-    ln -s ~/.dotfiles/bash/bashrc_linux ~/.bashrc
-    ln -s ~/.dotfiles/bash/bash_git_linux ~/.bash_git
-    ln -s ~/.dotfiles/bash/bash_aliases ~/.bash_aliases
-    ln -s ~/.dotfiles/gitconfig.linux ~/.gitconfig
-    ln -s ~/.dotfiles/tmux.conf.linux ~/.tmux.conf
-    ln -s ~/.dotfiles/slate.js ~/.slate.js
-    ln -s ~/.dotfiles/ideavimrc ~/.ideavimrc
-    ln -s ~/.dotfiles/spacemacs ~/.spacemacs
-    ln -s ~/.dotfiles/yabairc ~/.yabairc
+    ln -sf ~/.dotfiles/bashrc/bashrc_linux ~/.bash_profile
+    ln -sf ~/.dotfiles/bashrc/bashrc_linux ~/.bashrc
+    ln -sf ~/.dotfiles/bashrc/bash_git_linux ~/.bash_git
+    ln -sf ~/.dotfiles/bashrc/bash_aliases ~/.bash_aliases
+    ln -sf ~/.dotfiles/gitconfig.linux ~/.gitconfig
+    ln -sf ~/.dotfiles/tmux.conf.linux ~/.tmux.conf
+    ln -sf ~/.dotfiles/spacemacs ~/.spacemacs
 }
 
-if [ "${2}" = "-f" ]; then
-    remove_symlinks
-elif [ ! -z "${2}" ] && [ "${2}" != "-f" ]; then
-    echo "\n\tOnly accepted flag for second param is 'f'\n"
-    exit 1
-fi
+
+# find config -maxdepth 1 -mindepth 1 -type d -exec ln -sf ../'{}' ~/.config/ \;
+cp -a config/. ~/.config
 
 if [ -z "${1}" ]; then
     echo "\n\tNeed to specify which OS, '-m' for Mac, '-l' for Linux\n"
@@ -55,6 +34,6 @@ elif [ "${1}" = "-m" ]; then
 elif [ "${1}" = "-l" ]; then
     link_linux_symlinks
 else
-    echo "\n\tValid flags for first param are 'l' or 'm'\n"
+    echo "\n\tValid flags for param are 'l' or 'm'\n"
 fi
 
