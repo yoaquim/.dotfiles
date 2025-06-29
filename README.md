df98826183ea78ec72f2ecbf5e3317e2110d5aa1
Yoaquim Cintrón <yoaquim@outlook.com>
2025-06-29 12:06:25 -0400

    v2.0 Update

--- README.md
+++ README.md
@@ -4,34 +4,26 @@
 
 Contains all my personal dotfiles and dirs:
 
- - Bash Profile
- - Git Configuration
- - Slate Configuration
- - Config Directory (where some applications set configuration)
+ - Bash Profile + subfiles for organization
  - Tmux Configuration
- - IntelliJ vimrc settings and CLI launcher
- - i3wm & i3blocks for Linux
+ - Git Configuration
+ - Config Directory
+ - Reactangle Configuration
 
 ## Setup
 
 - Clone this repo into your `~/` directory:
  ```Shell
  cd ~/
-
  git clone https://github.com/cintron/.dotfiles.git
  ```
 
-- Run `setup.sh` to setup dotfiles. Need to specify for mac(m) or linux(l):
+- Run `install.sh` to init
 
  ```
  cd  ~/.dotfiles
- ./setup.sh -l
+ ./install.sh
  ```
-
-## Mac Post-setup
-
-### Homebrew
-`brewlist.txt` contains the desired packages for a base setup. Running the `install_brew_list.sh` script will install the packages in that list.
 
 
 ### Bash
@@ -40,24 +32,10 @@ Contains all my personal dotfiles and dirs:
 Mac OSX now ships with zsh as the default shell. In order to change it, add the correct homebrew bash path to `/etc/shells` and then run `chsh` accordingly:
 
 ```shell
-# If Intel-based Mac
-sudo sh -c 'echo /usr/local/bin/bash >> /etc/shells'
-chsh -s /usr/local/bin/bash
-echo 'eval "$(/usr/local/bin/brew shellenv)"' ~/.bash_local
-
-# If M1 chip Mac
 sudo sh -c 'echo /opt/homebrew/bin/bash >> /etc/shells'
 chsh -s /opt/homebrew/bin/bash
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' ~/.bash_local
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' ~/.config/bashrc/bashrc_local
 ```
 
 [This thread](https://apple.stackexchange.com/questions/291287/globstar-invalid-shell-option-name-on-macos-even-with-bash-4-x) has some more context if things don't work.
 
-
-## Linux Post-setup
-
-This guide helps with Lenovo fingerprint integration: https://forum.kde.org/viewtopic.php?f=309&t=175570
-
-### Spacemacs
-
-For Spacemacs keybindings, this article helped: https://www.emacswiki.org/emacs/ModifierKeys
---
+++ install.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+print() {
+    local MESSAGE="${1}"  
+    echo -e "\n\e[1;33m---------------------[INFO]\e[0m ${MESSAGE}..."
+}
+
+link_symlinks() {
+    ln -sf ~/.dotfiles/config/bashrc/bashrc ~/.bash_profile
+    ln -sf ~/.dotfiles/config/bashrc/bashrc ~/.bashrc
+    ln -sf ~/.dotfiles/config/bashrc/bashrc_aliases ~/.config/bashrc/bashrc_aliases
+    ln -sf ~/.dotfiles/config/bashrc/bashrc_functions ~/.config/bashrc/bashrc_functions
+    ln -sf ~/.dotfiles/config/bashrc/bashrc_tools ~/.config/bashrc/bashrc_tools
+    ln -sf ~/.dotfiles/config/bashrc/bashrc_git ~/.config/bashrc/bashrc_git
+    ln -sf ~/.dotfiles/config/tmux/tmux.conf ~/.config/tmux/tmux.conf
+    ln -sf ~/.dotfiles/config/gitconfig ~/.gitconfig
+}
+
+setup_tmux() {
+    if [ ! -f ~/.config/tmux/plugins/tpm/README.md ]; then
+        git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+    fi
+}
+
+setup_base_16() {
+    rm -rf ~/.config/base16-shell
+    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+}
+
+setup_brew_taps() {
+    brew tap homebrew/cask-fonts
+    brew tap chrokh/tap
+}
+
+install_brew_list() {
+    brew install awscli
+    brew install bash
+    brew install bash-completion
+    brew install coreutils
+    brew install diff-so-fancy
+    brew install git
+    brew install jq
+    brew install node
+    brew install nvm
+    brew install the_silver_searcher
+    brew install tmux
+    brew install vim
+    brew install tldr
+    brew install gh
+    brew install neovim
+}
+
+install_brew_casks() {
+    brew install --cask alfred
+    brew install --cask spotify
+    brew install --cask whatsapp
+    brew install --cask docker
+    brew install --cask postman
+    brew install --cask slack
+    brew install --cask kitty
+    brew install --cask todoist
+    brew install --cask adobe-creative-cloud
+    brew install --cask google-chrome
+    brew install --cask rectangle
+}
+
+install_brew_fonts() {
+    brew install font-hack
+    brew install font-source-code-pro
+    brew install font-fira-code
+    brew install font-jetbrains-mono
+    brew install font-fantasque-sans-mono
+    brew install font-ubuntu-mono
+    brew install font-space-mono
+    brew install font-inconsolata
+}
+
+
+print "Setting up symlinks"
+link_symlinks
+
+print "Setting up tmux"
+setup_tmux
+
+print "Setting up base16"
+setup_base_16
+
+print "Tapping brew taps"
+setup_brew_taps
+
+print "Installing brew list"
+install_brew_list
+
+print "Installing brew casks"
+install_brew_casks
+
+print "Installing brew fonts"
+install_brew_fonts
+
+# ============================
+# FINISHED
+# ============================
+echo -e "\n\e[1;32m[SUCCES]\e[0m DONE.\n"
+
