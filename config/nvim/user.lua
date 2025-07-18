96a822ab556905999f63a7edc43075b01284a928
Yoaquim Cintrón <yoaquim@outlook.com>
2025-07-17 20:25:34 -0400

    Add nvim-spectre for search and replace

--- config/nvim/user.lua
+++ config/nvim/user.lua
@@ -246,6 +246,21 @@ return {
     },
   },
 
+  -- Spectre - Search and Replace
+  -- ───────────────────────────────────────────────────
+  {
+    "nvim-pack/nvim-spectre",
+    build = false,
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
+    keys = {
+      { "<leader>S", "<cmd>lua require('spectre').toggle()<cr>", desc = "Toggle Spectre" },
+      { "<leader>sw", "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", desc = "Search current word" },
+      { "<leader>sw", "<cmd>lua require('spectre').open_visual()<cr>", mode = "v", desc = "Search current word" },
+      { "<leader>sp", "<cmd>lua require('spectre').open_file_search({select_word=true})<cr>", desc = "Search on current file" },
+    },
+  },
+
   -- Color Schemes
   -- ───────────────────────────────────────────────────
   { "RRethy/base16-nvim" },
